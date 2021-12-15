2ad6ff134a4ee99c91ac4abb8a2dab7eeaae89b3
hamzaiy90 <hamzaiy90@hotmail.com>
2021-12-15 15:27:14 +0000

    further....changes

--- db/index.js
+++ db/index.js
@@ -1,19 +1,23 @@
 import pg from 'pg';
 
-console.log('pg', pg);
-
 const pool = new pg.Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
   // user: 'newuser',
   // host: 'localhost',
   // database: 'postgres',
   // password: 'password',
   // port: process.env.PORT ||,
-  connectionString: process.env.DATABSE_URI,
+  // connectionString: process.env.DATABSE_URI,
   // ssl: {
   //   rejectUnauthorized: true,
   // },
 });
 
+console.log('pg', pg);
 export function query(text, params) {
   return pool.query(text, params);
 }
