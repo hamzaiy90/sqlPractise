71a453707c874c92cf413ceaa829ffa5386b1604
hamzaiy90 <hamzaiy90@hotmail.com>
2021-12-16 13:49:57 +0000

    should work

--- db/index.js
+++ db/index.js
@@ -1,20 +1,20 @@
 import pg from 'pg';
 
 const pool = new pg.Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // host: process.env.DB_HOST,
+  // port: process.env.DB_PORT,
+  // database: process.env.DB_DATABASE,
   // user: 'newuser',
   // host: 'localhost',
   // database: 'postgres',
   // password: 'password',
   // port: process.env.PORT ||,
-  // connectionString: process.env.DATABSE_URI,
-  // ssl: {
-  //   rejectUnauthorized: true,
-  // },
+  connectionString: process.env.DATABSE_URL,
+  ssl: {
+    rejectUnauthorized: true,
+  },
 });
 
 console.log('pg', pg);
