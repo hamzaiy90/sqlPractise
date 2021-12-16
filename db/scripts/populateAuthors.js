a46517f5fc215c31d2f54f0cd8ac9100f08f416a
hamzaiy90 <hamzaiy90@hotmail.com>
2021-12-16 15:08:43 +0000

    checking authentication for git

--- db/scripts/populateAuthors.js
+++ db/scripts/populateAuthors.js
@@ -1,20 +1,21 @@
 import { books, authors } from '../../libs/data.js';
 
 import { query } from '../index.js';
-const populateTables = () => {
-  authors.forEach(async ({ first_name, last_name }) => {
-    return await query(
-      `INSERT into authors(first_name, last_name) VALUES($1, $2)`,
-      [first_name, last_name]
-    );
-  });
-  books.forEach(async ({ author_id, title, publishedDate }) => {
-    console.log({ author_id, title, publishedDate });
-    return await query(
-      `INSERT into books(author_id, title, published_date) VALUES($1, $2, $3)`,
-      [author_id, title, publishedDate]
-    );
-  });
+const populateTables = async () => {
+  // authors.forEach(async ({ first_name, last_name }) => {
+  //   return await query(
+  //     `INSERT into authors(first_name, last_name) VALUES($1, $2)`,
+  //     [first_name, last_name]
+  //   );
+  // });
+  await query('DROP TABLE authors, books;');
+  // books.forEach(async ({ author_id, title, publishedDate }) => {
+  //   console.log({ author_id, title, publishedDate });
+  //   return await query(
+  //     `INSERT into books(author_id, title, published_date) VALUES($1, $2, $3)`,
+  //     [author_id, title, publishedDate]
+  //   );
+  // });
   console.log('created');
 };
 
